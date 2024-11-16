091a8d9c733f0496be24ff4c1e7246e3f1a4e64a
juliana <juliana.li@csedge.org>
2024-11-16 11:39:55 -0800

    todo list (create and read created)

---
+++ app.py
@@ -0,0 +1,43 @@
+from tinydb import TinyDB, Query
+db = TinyDB('./db.json')
+
+todos = Query()
+
+while True:
+    print("1. Create")
+    print("2. Read")
+    print("3. Update")
+    print("4. Delete")
+    
+    userinput = input("Enter your command: ")
+    if userinput.lower == 'create' or userinput == "1":
+        task = input("What task would you like to add? ")
+        importance = input("Of what importance is this? (High, Medium, Low) ")
+        due = input("When is this due? ")
+        create = db.insert({'task': task, 'importance': importance, "Due Date" : due})
+    if userinput.lower == 'read' or userinput == "2":
+        asklevel = input("Would you like to read all or one? (Enter all or one) ")
+        if asklevel.lower == 'all':
+            read = db.all()
+            print(read)
+        elif asklevel.lower == 'one':
+            itemlist = input("Which task are you looking for? (Enter the name of the task) ")
+            read = db.search(todos.task == itemlist)
+            print(read)
+        else:
+            print("You didn't enter 'add' or 'one'")
+    if userinput.lower == 'update' or userinput == "3":
+        type = input("What do you want to update? (task, importance, or due) ")
+        if type.lower == "task":
+            task = input("Which task do you want to update?")
+            nameTask = input("What do you want to change it to?")
+            update = db.update({'task': })
+        elif type.lower == "importance":
+        elif type.lower == "due":
+    
+
+
+# create = db.insert
+# read = db.all()
+# update = db.update({'changed data'}, where you want to update)
+# delete = db.remove
